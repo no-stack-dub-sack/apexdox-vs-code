231bb4792852e74ef10eabfbf08b7ca099c21245
Peter Weinberg <pweinberg633@gmail.com>
2019-06-04 21:52:20 -0400

    fix(generators): fixes to SeeLinkGenerator:   * replace user label with created label to preserve method/class name casing in UI   * abstract method/overload matching logic into own method to reuse for matching child class methods   * fix child class overload selector omission; overloaded child class methods can now be properly linked

--- src/engine/generators/SeeLinkGenerator.ts
+++ src/engine/generators/SeeLinkGenerator.ts
@@ -2,6 +2,12 @@ import * as Models from '../../common/models';
 import ApexDocError from '../../common/ApexDocError';
 import Utils from '../../common/Utils';
 
+interface IMatchResult {
+    foundMatch: boolean;
+    selector: string;
+    methodName: string;
+}
+
 class SeeLinkGenerator {
 
     private static isMarkdownURL(str: string): boolean {
@@ -57,15 +63,15 @@ class SeeLinkGenerator {
             // to specify a method other than the 1st. Otherwise SomeClass.SomeMethod is fine
             let overloadSelector = 0;
             if (/.*\[\d+\]$/.test(qualifier)) {
-                let i = qualifier.lastIndexOf('[');
+                const i = qualifier.lastIndexOf('[');
                 // isolate the number inside the brackets
-                let selector = qualifier.substring(i+1, qualifier.length - 1);
+                const selector = qualifier.substring(i+1, qualifier.length - 1);
                 overloadSelector = Number(selector);
                 // strip away the suffix from the qualifier
                 qualifier = qualifier.substring(0, i);
             }
 
-            let parts = qualifier.split('.').map(p => p.toLowerCase());
+            const parts = qualifier.split('.').map(p => p.toLowerCase());
 
             if (!parts.length || parts.length > 3) {
                 throw new ApexDocError(`Qualifier '${qualifier}' is invalid. ${ApexDocError.INVALID_SEE_QUALIFIER}`);
@@ -76,46 +82,30 @@ class SeeLinkGenerator {
 
             // 4.A) if first qualifier matches class name, begin search: We've
             // made the model map in all lowercase to avoid case mis-matching
-            let model = modelMap.get(parts[0]);
+            const model = modelMap.get(parts[0]);
 
             if (model) {
                 // if only a single qualifier, stop here
                 if (parts.length === 1) {
                     href = model.name + '.html';
+                    qualifier = model.name;
                     foundMatch = true;
                 }
 
                 // 4.B) otherwise keep searching for a match for the second qualifier as long as
                 // model is not an enum model, in which case there is no searching left to do
                 else if (parts.length >= 2 && model.modelType !== Models.ModelType.ENUM) {
-                    let Class = <Models.ClassModel>model;
-                    let methods = Class.methods;
-                    let childClasses = Class.childClassMap;
+                    const Class = <Models.ClassModel>model;
+                    const methods = Class.methods;
+                    const childClasses = Class.childClassMap;
 
-                    let methodNum = 0;
-                    for (let method of methods) {
-                        if (method.name.toLowerCase() === parts[1]) {
-                            // use actual class/method name to create link to avoid case issues
-                            href = Class.name + '.html#' + Class.name + '.' + method.name;
-                            // no overload selector, we've made a match!
-                            if (overloadSelector === 0) {
-                                foundMatch = true;
-                                break;
-                            }
-                            // If there's an overload suffix to take into account
-                            // ensure that many overloads of the method actually
-                            // exist before committing to the method link.
-                            else if (overloadSelector > 0 && methodNum !== overloadSelector) {
-                                methodNum++;
-                                continue;
-                            }
-                            // confirmed overload exists. Match!!
-                            else if (methodNum === overloadSelector) {
-                                href += '_' + overloadSelector;
-                                foundMatch = true;
-                                break;
-                            }
-                        }
+                    // attempt to match method, and select correct overload if required
+                    const matchResults = this.methodMatcher(methods, parts[1], overloadSelector);
+                    if (matchResults.foundMatch) {
+                        qualifier = `${Class.name}.${matchResults.methodName}`;
+                        href = `${Class.name}.html#${Class.name}.${matchResults.methodName}`;
+                        href += matchResults.selector; // append overload selector
+                        foundMatch = true;
                     }
 
                     // 4.C) if after searching methods a match hasn't been found
@@ -123,29 +113,29 @@ class SeeLinkGenerator {
                     if (!foundMatch) {
                         // ApexDoc2 stores child class name as 'OuterClass.InnerClass'
                         // recreate that format below to try to make the match with
-                        let childClassName = parts[0] + '.' + parts[1];
-                        let childClass = childClasses.get(childClassName);
+                        const maybeChildClassName = parts[0] + '.' + parts[1];
+                        const childClass = childClasses.get(maybeChildClassName);
 
                         if (childClass) {
-                            let nameParts = childClass.name.split("\\.");
+                            const [ parentClassName, childClassName ] = childClass.name.split('.');
                             // 4.D) If match, and only 2 parts, stop here.
                             if (parts.length === 2) {
-                                // to ensure the link works, use actual name rather than
-                                // user provided parts in case casing doesn't match
-                                href = nameParts[0] + '.html#' + nameParts[0] + '.' + nameParts[1];
+                                // to ensure the link works, use model names rather than user provided
+                                // qualifiers in case casing doesn't match. Label will match user's
+                                // qualifier, but by using model names, we ensure proper casing.
+                                href = `${parentClassName}.html#${parentClassName}.${childClassName}`;
+                                qualifier = `${parentClassName}.${childClassName}`;
                                 foundMatch = true;
                             }
                             // 4.E) Otherwise, there must be 3 parts
                             // attempt to match on child class method.
                             else {
-                                let childMethods = childClass.methods;
-                                for (let method of childMethods) {
-                                    if (method.name.toLowerCase() === parts[2]) {
-                                        // same as above, use actual name to avoid casing issues
-                                        href = nameParts[0] + '.html#' + childClass.name + '.' + method.name;
-                                        foundMatch = true;
-                                        break;
-                                    }
+                                const matchResults = this.methodMatcher(childClass.methods, parts[2], overloadSelector);
+                                if (matchResults.foundMatch) {
+                                    qualifier = `${childClass.name}.${matchResults.methodName}`;
+                                    href = `${parentClassName}.html#${childClass.name}.${matchResults.methodName}`;
+                                    href += matchResults.selector; // append overload selector
+                                    foundMatch = true;
                                 }
                             }
                         }
@@ -167,6 +157,50 @@ class SeeLinkGenerator {
 
         // 6) collect links / spans and join back into a single string
         return links.join(', ');
+    }
+
+    /**
+     * Search through methods for a match to the provided qualifier. If a match is made
+     * and an overload selector is provided, continue iterating to see if that overload
+     * exists, and match on it if it does. If it does not, report that no match is made.
+     *
+     * @param methods The methods to search for a match through
+     * @param matcher The user provided qualifier to match against a method name
+     * @param overloadSelector The user provided indicator of the method overload number
+     */
+    private static methodMatcher(methods: Models.MethodModel[], matcher: string, overloadSelector: number): IMatchResult {
+        let methodNum = 0;
+        let foundMatch = false;
+        let selector = '', methodName = '';
+
+        for (let method of methods) {
+            if ((methodName = method.name.toLowerCase()) === matcher) {
+                // no overload selector, we've made a match!
+                if (overloadSelector === 0) {
+                    foundMatch = true;
+                    break;
+                }
+                // If there's an overload suffix to take into account
+                // ensure that many overloads of the method actually
+                // exist before committing to the method link.
+                else if (overloadSelector > 0 && methodNum !== overloadSelector) {
+                    methodNum++;
+                    continue;
+                }
+                // confirmed overload exists. Match!!
+                else if (methodNum === overloadSelector) {
+                    selector = '_' + overloadSelector;
+                    foundMatch = true;
+                    break;
+                }
+            }
+        }
+
+        return {
+            foundMatch,
+            methodName,
+            selector
+        };
     }
 }
 
